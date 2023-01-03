2cfd1d2194862451b97484b1a360f0094988f495
Pankaj Ram <116742441+pankaj1101@users.noreply.github.com>
2023-01-03 20:36:57 +0530

    Update hashmap.java

--- hashmap.java
+++ hashmap.java
@@ -4,29 +4,36 @@ import java.util.Map;
 public class hashmap {
 
     public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
 
-        HashMap<String, String> hash = new HashMap<>();
+        // Different ways to create hashmap
+        // HashMap<String, Integer> map = new HashMap<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // HashMap<Integer, String> map = new HashMap<>();
 
-        hash.put("A", "1");
-        hash.put("B", "2");
-        hash.put("C", "3");
-        hash.put("D", "4");
-        hash.put("E", "5");
-        
-        System.out.println(hash);
+        // To insert values into Hashmap
+        map.put("A", "1");
+        map.put("B", "2");
+        map.put("C", "3");
+        map.put("D", "4");
+        map.put("E", "5");
 
-        // To check key present or not in the hashmap..
-        if (hash.containsKey("A")) {
+        //To print hashmap 
+        System.out.println(map);
+
+        // To check key present or not in the map..
+        if (map.containsKey("A")) {
             System.out.println("key is present");
         } else {
             System.out.println("key is not present");
         }
 
-        // To get the values from hashmap
-        System.out.println(hash.get("E"));
+        // To get the values from hashmap using key
+        System.out.println(map.get("E"));
 
         // To get key value pair..
-        for (Map.Entry<String, String> e : hash.entrySet()) {
+    
+        for (Map.Entry<String, String> e : map.entrySet()) {
             System.out.println(e.getKey() + "=" + e.getValue());
         }
 
